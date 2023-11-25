6aa986575905e7480ca2abff9950c7e4c1e0d6e4
sonlamdev <152028314+sonlamdev@users.noreply.github.com>
2023-11-26 02:13:46 +0700

    Delete License Server directory

--- License Server/config/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const db = new Sequelize('simple-licensor', 'admin', '12345', {
-    host: 'localhost',
-    dialect: 'postgres'
-  });
-
-module.exports = db
