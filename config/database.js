f7f668d667d321b010f9283e45dbe975571584b9
sonlamdev <152028314+sonlamdev@users.noreply.github.com>
2023-11-26 14:12:23 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,5 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('simple-licensor', 'admin', '12345', {
-    host: 'localhost',
-    dialect: 'postgres'
-  });
+const db = new Sequelize('postgres://simplelicensor_user:b6j0RTVWnU1hFt9GMRxgDZLyPNPoJGew@dpg-clh51l58td7s73bmfuqg-a.singapore-postgres.render.com/simplelicensor');
 
 module.exports = db
